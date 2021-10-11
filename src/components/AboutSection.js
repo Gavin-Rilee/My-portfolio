1440a7323a53eeaf6b9ef24fb06d71ac21bc5931
Gavin Rilee <grilee12@gmail.com>
2021-10-11 12:07:43 -0400

    quality-styles

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import home1 from '../img/home1.png';
 //Styled 
 import styled from 'styled-components'
+import { About, Descripton,Image,Hide } from './styles';
 
 
 const AboutSection = () => {
@@ -29,33 +30,5 @@ const AboutSection = () => {
     )
 }
 
-//Styled components
-const About = styled.div`
-min-height: 90vh;
-display: flex;
-align-items: center;
-justify-content: center;
-padding: 5rem 10rem;
-color: white;
-;
-`
-const Descripton = styled.div`
-flex: 1;
-padding-right:5rem;
-h2{
- font-weight:lighter;
-}
-`
-const Image = styled.div`
-flex: 1;
-overflow: hidden;
-img{
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-}
-`
-const Hide = styled.div`
-  overflow: hidden;
-`
+
 export default AboutSection
--- src/components/Qualities.js
+++ src/components/Qualities.js
@@ -5,48 +5,81 @@ import diaphragm from "../img/diaphragm.svg";
 import money from "../img/money.svg";
 import teamwork from "../img/teamwork.svg";
 import home2 from "../img/home2.png";
+//styles
+import { About, Descripton,Image } from './styles';
+import styled from 'styled-components'
 
 const Qualities = () => {
     return (
-        <div className="qualities">
-            <div className="description">
+        <Quality>
+            <Descripton>
                 <h2>High <span>quality</span>  skills</h2>
-                <div className="cards">
-                    <div className="card">
+                <Cards>
+                    <Card>
                         <div className="icon">
                             <img src={clock} alt=""/>
                             <h3>Efficient</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img src={teamwork} alt=""/>
                             <h3>Teamwork</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img src={diaphragm} alt=""/>
                             <h3>Diaphragm</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-                    </div>
-                    <div className="card">
+                    </Card>
+                    <Card>
                         <div className="icon">
                             <img src={money} alt=""/>
                             <h3>Affordable</h3>
                         </div>
                         <p>Lorem ipsum dolor sit amet, consectetur adip</p>
-                    </div>
-                </div>
-            </div>
-            <div className="image">
+                    </Card>
+                </Cards>
+            </Descripton>
+            <Image>
                 <img src={home2} alt ="fix later" />
-            </div>
-        </div>
+            </Image>
+        </Quality>
     )
 }
+
+
+const Quality = styled(About)`
+    h2{
+        padding-bottom: 5rem;
+
+    }
+    p{
+        width: 70%;
+        padding: 2rem 0rem 4rem 0rem;
+    }
+`
+const Cards = styled.div`
+display: flex;
+flex-wrap: wrap;
+`
+const Card = styled.div`
+flex-basis: 20rem;
+.icon{
+display:flex;
+align-items: center;
+}
+h3{
+    margin-left: 1rem;
+    background: white;
+    color:black;
+    padding: 1rem
+
+}
+`
 
 export default Qualities
